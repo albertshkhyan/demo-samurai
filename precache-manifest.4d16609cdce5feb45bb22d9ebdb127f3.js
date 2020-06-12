62f4b6d9ce42fe25fa173301f192bc92dcaa6d24
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-06-13 01:28:04 +0400

    create static files

---
+++ precache-manifest.4d16609cdce5feb45bb22d9ebdb127f3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797b302b14cfe8a7301dcc633f524050",
+    "url": "/albertshkhyan/demo-samurai.git/index.html"
+  },
+  {
+    "revision": "59bb48eab0fe63c73c6c",
+    "url": "/albertshkhyan/demo-samurai.git/static/css/4.0e14f1b2.chunk.css"
+  },
+  {
+    "revision": "565f6ee0f516b590e5e1",
+    "url": "/albertshkhyan/demo-samurai.git/static/css/5.4de88b29.chunk.css"
+  },
+  {
+    "revision": "fe615932d448759fff16",
+    "url": "/albertshkhyan/demo-samurai.git/static/css/6.f447b013.chunk.css"
+  },
+  {
+    "revision": "228d7cf845594687a38c",
+    "url": "/albertshkhyan/demo-samurai.git/static/css/7.8a3faa3e.chunk.css"
+  },
+  {
+    "revision": "c210070ac67e2db09277",
+    "url": "/albertshkhyan/demo-samurai.git/static/css/main.6589dcab.chunk.css"
+  },
+  {
+    "revision": "951384ab2c020a256323",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/0.ac44768d.chunk.js"
+  },
+  {
+    "revision": "5478e885749e527e8f0b",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/3.374a06ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/3.374a06ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bb48eab0fe63c73c6c",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/4.8709c73a.chunk.js"
+  },
+  {
+    "revision": "565f6ee0f516b590e5e1",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/5.a46f6159.chunk.js"
+  },
+  {
+    "revision": "fe615932d448759fff16",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/6.5d2b6356.chunk.js"
+  },
+  {
+    "revision": "228d7cf845594687a38c",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/7.9f9dfe82.chunk.js"
+  },
+  {
+    "revision": "c210070ac67e2db09277",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/main.2579e739.chunk.js"
+  },
+  {
+    "revision": "f20ec57a3f8abda75392",
+    "url": "/albertshkhyan/demo-samurai.git/static/js/runtime-main.37bf8a59.js"
+  },
+  {
+    "revision": "b451b74dc076dec47b7f3963e5035e44",
+    "url": "/albertshkhyan/demo-samurai.git/static/media/61+cMK7E3AL._AC_SX522_.b451b74d.jpg"
+  },
+  {
+    "revision": "fafb2c2737ce2ac05f646d89781c7054",
+    "url": "/albertshkhyan/demo-samurai.git/static/media/f121f6ac7bf42728e49b1b723a331d46.fafb2c27.jpg"
+  },
+  {
+    "revision": "7ca1a465675530f81a9bed394bee1828",
+    "url": "/albertshkhyan/demo-samurai.git/static/media/giphy.7ca1a465.webp"
+  },
+  {
+    "revision": "98fcd4219a7f866f1db78263618894e9",
+    "url": "/albertshkhyan/demo-samurai.git/static/media/preloader_1.98fcd421.svg"
+  },
+  {
+    "revision": "95de20211796d25cc40d4dc00c81aa3e",
+    "url": "/albertshkhyan/demo-samurai.git/static/media/samurai-logo-collection_99536-106.95de2021.jpg"
+  },
+  {
+    "revision": "6db09181ed93f7e8f2dac478623e3629",
+    "url": "/albertshkhyan/demo-samurai.git/static/media/users.6db09181.png"
+  }
+]);
